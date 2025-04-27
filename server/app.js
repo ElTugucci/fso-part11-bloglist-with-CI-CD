5fe014b820038a72d5554d7ff464e3c900e40e72
Anton Tugushi <anton.tugushi@gmail.com>
2025-04-28 01:45:10 +0300

    Healthcheck action

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ app.use(middleware.userExtractor);
 app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', usersRouter);
-app.use('api/health', healthRouter);
+app.use('/api/health', healthRouter);
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing');
